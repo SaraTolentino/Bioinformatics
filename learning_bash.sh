2670ff3c8802c5e3fd0e92fdfebc983eedf5aec8
SaraTolentino <Sararachel.tolentinocaceres@wmich.edu>
2025-01-30 11:23:15 -0500

    Add files via upload
    Assignment 2

---
+++ learning_bash.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+greeting="hello"
+name="sara"
+
+echo "$greeting $name!"
+echo "This script is helping you learn bash"
+
+mkdir -p ~/Desktop/bioinformatics/bash_scripts
+#Step 3
+mv ~/Desktop/bash_template.txt ~/Desktop/bioinformatics/bash_scripts
+#Step 4
+mv ~/Desktop/bioinformatics/bash_scripts/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/bash_template.sh
+
